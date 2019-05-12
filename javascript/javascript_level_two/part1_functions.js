b47f0cf983e9b42be73467971b619da50846bfa1
Andrew Rietz <andrew.rietz@gmail.com>
2019-05-11 22:57:53 -0600

    Started javascript level 2

---
+++ javascript/javascript_level_two/part1_functions.js
@@ -0,0 +1,13 @@
+// The general syntax is:
+//
+// function name(parameter 1, parameter 2, ...){
+//   Code to be executed
+// }
+
+function hello(){
+  console.log("Hello world!");
+}
+
+function helloYou(name) {
+  console.log("Hello " + name);
+}
